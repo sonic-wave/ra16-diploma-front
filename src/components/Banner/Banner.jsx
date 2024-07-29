0377563e86b5d8d682c5a59f5321b188738bae5c
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2024-07-29 18:19:56 +0300

    change img urls in banner and header

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import './Banner.css';
 export const Banner = () => {
     return (
         <div className="banner">
-            <img src={`${import.meta.env.BASE_URL}img/banner.jpg`} className="img-fluid" alt="К весне готовы!" />
+            <img src={`${import.meta.env.BASE_URL}/img/banner.jpg`} className="img-fluid" alt="К весне готовы!" />
             <h2 className="banner-header">К весне готовы!</h2>
         </div>
     )
